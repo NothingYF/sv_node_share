3e7b322fb6a4261dfaa8e776555cdcf5a23d4a44
杨帆 <yf@scsvision.com>
2017-09-11 10:40:59 +0800

    v1.1.1 新增config更新通知;修改logger全局level，方便动态调试

--- example/example.config.js
+++ example/example.config.js
@@ -1,10 +1,13 @@
 const config = require('../index').config;
 const logger = require('../index').logger('config');
+const LOG_LEVELS = { DEBUG : 0, INFO : 1, WARN : 2, ERROR : 3, OPERATOR : 4};
 
-
-var cfg = config.load(__dirname + '/config.yml', ['type', 'sn']);
+var cfg = config.load(__dirname + '/config.yml', ['type', 'sn'], (data)=>{
+    logger.setLevel(data.debug ? LOG_LEVELS.DEBUG : LOG_LEVELS.INFO);
+});
 
 
 setInterval(()=>{
    logger.debug(cfg);
+   logger.info('info');
 }, 5000);
